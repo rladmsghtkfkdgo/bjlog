3114e75a79c3e56d6702c45d7014a5ca60c917bb
rladmsghtkfkdgo <hearang3180@naver.com>
2024-03-20 06:26:01 +0900

    백준10950번 해결

---
+++ BJ10950.py
@@ -0,0 +1,12 @@
+
+#개뿔 다 필요없었음 초기에 내가 이미 다 생각함 예시 쉬발것
+#T = input()
+a = int(input())
+
+for i in range(a):
+    c, b = map(int,input().split())
+    print(c+b)
+
+
+#for (A , B) in range(T):
+    #A, B = map(int,input().split())
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('hi')
+#a=5
+#b=[]
+a = int(input())
+
+for i in range(a):
+    c, b = map(int,input().split())
+    print(c+b)
